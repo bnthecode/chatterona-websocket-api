57277c07f480b9478e9edbe4b19b9bde09551a24
Brandon Counts <bran.counts1@gmail.com>
2021-02-20 00:17:28 -0500

    websocket stuffz

---
+++ server/WebsocketRegistry.js
@@ -0,0 +1,220 @@
+import { v4 } from "uuid";
+
+class WebsocketRegistry {
+  constructor(server) {
+    this.websocketServer = server;
+    this.channels = {};
+    this.connectionPairs = {};
+  }
+
+  createContext = (subscriber, publisher) => {
+    try {
+      this.publisher = publisher;
+      this.subscriber = subscriber;
+      this.initializeEventListeners(subscriber);
+    } catch (error) {
+      console.log("error creating websocket pubsub", error.message);
+    }
+  };
+
+  initializeEventListeners = (subscriber) => {
+    try {
+      this.websocketServer.on("request", (request) => {
+        this.handleWebsocketRequest(request);
+      });
+
+      subscriber.on("message", (cache, message) => {
+        switch (cache) {
+          case "message-cache":
+            return this.handleOutgoingWebsocketMessage(message);
+          case "channel-typer":
+            return this.handleOutgoingWebsocketMessage(message);
+          case "existing-connections":
+            return this.cleanUpOpenSockets(message);
+        }
+      });
+
+      subscriber.subscribe("message-cache");
+      subscriber.subscribe("existing-connections");
+    } catch (error) {
+      console.log("error initializing websocket listeners", error.message);
+    }
+  };
+
+  handleWebsocketRequest = (request) => {
+    try {
+      console.log("handling initial connection");
+      const connection = request.accept(null, request.origin);
+      connection.CONNECTION_ID = v4();
+
+      connection.on("message", (message) => {
+        this.handleIncomingWebsocketMessage(message, connection);
+      });
+
+      connection.on("close", () => {
+        console.log("closing connection");
+      });
+    } catch (error) {
+      console.log("error proccessng websocket request", error.message);
+    }
+  };
+
+  handleIncomingWebsocketMessage = (message, connection) => {
+    console.log(message)
+    try {
+      const { channelId, type, offer, answer, candidate } = JSON.parse(
+        message.utf8Data
+      );
+   
+      switch (type) {
+        
+        //text chat
+        case "join-channel": {
+          return this.handleWebsocketJoinChannel(channelId, connection);
+        }
+        case "send-message": {
+          return this.handleWebsocketSendMessage(message.utf8Data);
+        }
+        case "channel-typer": {
+          return this.handleWebsocketAddChannelTyper(message.utf8Data);
+        }
+        // voice
+        case "join-voice": {
+          console.log("im adding myself into voice.., i have the channel id");
+          return;
+        }
+        case "candidate": {
+          console.log("candidate...", candidate);
+          connection.send(
+            JSON.stringify({
+              type: "candidate",
+              offer: offer,
+            })
+          );
+          return;
+        }
+
+        case "create-offer": {
+          console.log("offer.....", offer);
+          connection.send(
+            JSON.stringify({
+              type: "create-offer",
+              offer: offer,
+            })
+          );
+          return;
+        }
+
+        case "create-answer": {
+          console.log("answer.....", answer);
+          connection.send(
+            JSON.stringify({
+              type: "create-answer",
+              answer: answer,
+            })
+          );
+          return;
+        }
+
+        default: {
+          console.log(
+            "\x1b[31m%s\x1b[0m",
+            `websocket server did not handle message event for ${channelId}`
+          );
+        }
+      }
+    } catch (error) {
+      console.log("error processing websocket incoming message", error.message);
+    }
+  };
+
+  handleOutgoingWebsocketMessage = (message) => {
+    console.log("sending outgoing message");
+    try {
+      const { channelId } = JSON.parse(message);
+      const channel = this.channels[channelId];
+      const clients = channel ? channel.connectedClients : [];
+      clients.forEach((c) => c.send(message));
+    } catch (ex) {
+      console.log("ERR::" + ex);
+    }
+  };
+
+  handleWebsocketJoinChannel = (channelId, connection) => {
+    console.log("joining channel --> ", channelId);
+    try {
+      const channel = this.channels[channelId];
+      const connectedToOtherChannel = this.connectionPairs[
+        connection.CONNECTION_ID
+      ];
+      // each time we join a channel we create a new connection id...
+      // that connectionId is used to determine if we have already connected
+
+      this.channels = {
+        ...this.channels,
+        [channelId]: {
+          ...(channel || {}),
+          connectedClients: [
+            ...(channel ? channel.connectedClients : []),
+            connection,
+          ],
+        },
+      };
+      if (channel && connectedToOtherChannel) {
+        // distributed cache keeps all sockets aware of each other
+        // somehow make some checks so we dont store shit over and over
+        // like if this channel exists push it so everyone else knows it exists
+
+        this.publisher.publish(
+          "existing-connections",
+          JSON.stringify({
+            CONNECTION_ID: connection.CONNECTION_ID,
+            channelId: connectedToOtherChannel,
+          })
+        );
+      }
+
+      this.connectionPairs = {
+        ...this.connectionPairs,
+        [connection.CONNECTION_ID]: channelId,
+      };
+    } catch (error) {
+      console.log("error joining channel", error.message);
+    }
+  };
+
+  handleWebsocketSendMessage = (message) => {
+    try {
+      console.log("handling incoming message, of type message");
+      this.publisher.publish("message-cache", message);
+    } catch (error) {
+      console.log("error sending message", error.message);
+    }
+  };
+
+  handleWebsocketAddChannelTyper = (typer) => {
+    try {
+      this.publisher.publish("channel-typers", typer);
+    } catch (error) {
+      console.log("error sending message", error.message);
+    }
+  };
+
+  cleanUpOpenSockets = (redisUpdate) => {
+    // not too bad because channels dont really have a ridiculous amount of people listening at once..
+    const { CONNECTION_ID, channelId } = JSON.parse(redisUpdate);
+    // this is to update is to keep in sync with other servers
+    if (this.channels[channelId]) {
+      this.channels[channelId] = {
+        ...this.channels[channelId],
+        connectedClients: [
+          ...this.channels[channelId].connectedClients.filter(
+            (n) => n.CONNECTION_ID !== CONNECTION_ID
+          ),
+        ],
+      };
+    }
+  };
+}
+
+export default WebsocketRegistry;
--- server/server.js
+++ server/server.js
@@ -1,48 +1,35 @@
 import http from "http";
 import ws from "websocket";
 import redis from "redis";
-import { v4 as uuid } from "uuid";
+import WebsocketRegistry from "./WebsocketRegistry.js";
 const APPID = process.env.APPID;
-let channels = {};
-// when saving a connection save the index of the connections for fast deletion
-let indexMap = {};
 const WebSocketServer = ws.server;
 
-const subscriber = redis.createClient({
+export const subscriber = redis.createClient({
   port: 6379,
   host: "rds",
 });
 
-const publisher = redis.createClient({
+export const publisher = redis.createClient({
   port: 6379,
   host: "rds",
 });
 
-subscriber.on("subscribe", function (cache) {
-  console.log(
-    "\x1b[35m%s\x1b[0m",
-    `Server ${APPID} subscribed successfully to ${cache}`
-  );
-});
-
-subscriber.on("message", function (cache, message) {
-  try {
-    const { channelId } = JSON.parse(message);
-    const channel = channels[channelId];
-    const clients = channel ? channel.connectedClients : [];
-    clients.forEach((c) => c.send(message));
-  } catch (ex) {
-    console.log("ERR::" + ex);
-  }
-});
-
-subscriber.subscribe("message-cache");
 
 const httpserver = http.createServer();
 
-const websocket = new WebSocketServer({
+const wsServer = new WebSocketServer({
   httpServer: httpserver,
+  autoAcceptConnections: false,
 });
+
+try {
+  const websocketRegistry = new WebsocketRegistry(wsServer);
+  websocketRegistry.createContext(subscriber, publisher);
+  console.log('successfully initialized websocket registry....')
+} catch (error) {
+  console.log('error initializing websocket registry..', error.message)
+}
 
 httpserver.listen(5000, () => {
   console.log(
@@ -53,75 +40,3 @@ httpserver.listen(5000, () => {
     "-----"
   );
 });
-
-const getMessageType = (message, con) => {
-  const { channelId, message: updater, type } = JSON.parse(message.utf8Data);
-  switch (type) {
-    case "join-channel": {
-      console.log("\x1b[35m%s\x1b[0m", `joining channel ${channelId}`);
-      return handleSocketJoinChannel(channelId, con);
-    }
-    case "send-message": {
-      return publisher.publish("message-cache", message.utf8Data);
-    }
-    default: {
-      console.log(
-        "\x1b[31m%s\x1b[0m",
-        `websocket server did not handle message event for ${channelId}`
-      );
-    }
-  }
-};
-
-websocket.on("request", (request) => {
-  console.log("\x1b[33m%s\x1b[0m", `websocket initiated`);
-
-  const con = request.accept(null, request.origin);
-  con.on("message", (message) => {
-    getMessageType(message, con);
-  });
-  con.send(`Connected successfully to server ${APPID}`);
-  // need a way to clear open connections inside channel map
-});
-
-const handleSocketJoinChannel = (channelId, con) => {
-  const channel = channels[channelId];
-  const currentConnectionLength = channel ? channel.connectedClients.length : 0;
-  const connectionId = uuid();
-
-  channels = {
-    ...channels,
-    [channelId]: {
-      ...(channel || {}),
-      connectedClients: [...(channel ? channel.connectedClients : []), 
-        con,
-      ],
-    },
-  };
-  // keep index of connection for easy deletion
-  indexMap = {
-    ...indexMap,
-    [connectionId] : currentConnectionLength
-  }
-
-  con.on("close", () => {
-    const removalIdx = indexMap[connectionId];
-    const foundConnection = channel.connectedClients[removalIdx];
-    foundConnection.close();
-    channel.connectedClients.splice(indexMap[connectionId]);
-  });
-
-  console.log(
-    "\x1b[35m%s\x1b[0m",
-    `adding to channel sockets --> ${channelId}`
-  );
-};
-
-// Client
-// const ws = new WebSocket('ws://localhost:5000');
-// ws.onmessage = message => console.log(`Received: ${message.data}`);
-// const joinChannelMsg = { type: 'join-channel', channelId: '12345'};
-// ws.send(JSON.stringify(joinChannelMsg));
-// then -->
-// const messageToChannel = { type: 'send-message', message: 'Whats up guys?', channelId: '12345'};
-// ws.send(JSON.stringify(messageToChannel));
